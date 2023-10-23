7648544bc4d3b03e35c8ef47efe04a4dbd3ae3d7
FoxWritesBadCode <adamtylerfoxathome@gmail.com>
2023-10-23 16:45:06 -0400

    Add files via upload

---
+++ x2p.bat
@@ -0,0 +1,5 @@
+@ECHO Starting x2Prompt
+@ECHO OFF
+REM This .bat file is used to call the 'x2Prompt.py' via command line once the project folder is added to PATH
+REM The full path to x2Prompt.py
+python "C:\Python\x2Prompt\x2Prompt.py"
