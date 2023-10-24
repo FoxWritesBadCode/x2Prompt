b8733cc498d17332321d8a3d839608fb2fc431fc
Adam Fox <adam.fox@optm.com>
2023-10-24 16:09:02 -0400

    Updated args and bat

--- x2p.bat
+++ x2p.bat
@@ -2,4 +2,4 @@
 @ECHO OFF
 REM This .bat file is used to call the 'x2Prompt.py' via command line once the project folder is added to PATH
 REM The full path to x2Prompt.py
-python "C:\Python\x2Prompt\x2Prompt.py"
+python "C:\Python\x2Prompt\x2Prompt.py" %*
